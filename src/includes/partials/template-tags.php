5c1eedf259efa79d9f55219295ea004191a8d9c9
James Rosado <jamesros@boldgrid.com>
2023-02-15 12:34:05 -0500

    hotfix for featured images

--- src/includes/partials/template-tags.php
+++ src/includes/partials/template-tags.php
@@ -598,9 +598,7 @@ function bgtfw_get_featured_img_bg( $post_id, $theme_mod = false ) {
 function bgtfw_featured_img_bg( $post_id, $theme_mod = false ) {
 
 	// Note: See the docblock comment of this method for details regarding the escaping.
-	if ( 'show' === get_theme_mod( 'bgtfw_post_header_feat_image_display' ) ) {
-		echo bgtfw_get_featured_img_bg( $post_id, $theme_mod ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	}
+	echo bgtfw_get_featured_img_bg( $post_id, $theme_mod ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 }
 
 /**
