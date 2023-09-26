14f3d2bde4b1f004c849b9bb58dc55fca62035fa
James Rosado <jamesros@boldgrid.com>
2023-09-26 15:50:26 -0400

    update readme and tested to

--- src/assets/js/customizer/widget/section-update.js
+++ src/assets/js/customizer/widget/section-update.js
@@ -9,7 +9,7 @@ export default function() {
 				.filter( items => items.includes( 'sidebar' ) )
 				.map( control => control.replace( 'bgtfw_sidebar_', 'sidebar-widgets-' ) ),
 				sections = _.filter( window._wpCustomizeSettings.sections, ( section, id ) => id.includes( 'sidebar-widgets' ) );
-		
+
 				/*
 				 * Loop through the sections and determine which ones should or should not
 				 * be active based on the custom layout settings. However, the primarly-sidebar
